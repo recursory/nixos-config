d8c8fa3de3d74c302480bbceec2706dd4f669e55
recursory <olivianaomilund@gmail.com>
2024-12-01 10:39:53 -0800

    clean comments

--- configuration.nix
+++ configuration.nix
@@ -1,38 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
     ./hardware-configuration.nix
     ];
-
   boot = {
-    loader = { # Bootloader
+    loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
     supportedFilesystems = [ "ntfs" ];
   };
-  networking.hostName = "nixos"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-# Enable networking
-    networking.networkmanager.enable = true;
-
-# Set your time zone.
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
   time.timeZone = "America/Los_Angeles";
-
-# Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -44,11 +26,7 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
-
-# Enable CUPS to print documents.
   services.printing.enable = true;
-
-# Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -56,43 +34,30 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-# If you want to use JACK applications, uncomment this
-#jack.enable = true;
-
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
   };
-
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
   services.mozillavpn.enable = true;
-
-  users.users.recursory = { # Define a user account. Don't forget to set a password with ‘passwd’.
-    isNormalUser = true; # Debatable.
-      description = "Olivia Naomi Lund";
+  users.users.recursory = {
+    isNormalUser = true;
+# Debatable.
+    description = "Olivia Naomi Lund";
     extraGroups = [ "networkmanager" "wheel" "docker"];
     packages = with pkgs; [
-#  thunderbird
+      thunderbird
     ];
   }; 
-
   programs = {
-    firefox.enable = true; # Install firefox.
-      udevil.enable = true;
+    firefox.enable = true;
+    udevil.enable = true;
     steam = {
       enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
     };
   };
-  nixpkgs.config.allowUnfree = true; # Allow unfree packages
-
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-    environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
       wget
       git
       gparted
@@ -102,64 +67,34 @@
       obsidian
       milkytracker
       discord-canary
-      vesktop # for discord audio sharing
+      vesktop
       anki
       gimp
-# qbittorrent # currently insecure
       ffmpeg
       vlc
       gramps
       handbrake
       nodejs
       obs-studio
-      ];
-
-
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = {
-#   enable = true;
-#   enableSSHSupport = true;
-# };
-
-# List services that you want to enable:
-
+  ];
   services = {
     flatpak.enable = true;
-    openssh.enable = true; # Enable the OpenSSH daemon.
+    openssh.enable = true;
     xserver = {
-      xkb = { # Configure keymap in X11
+      xkb = {
         layout = "us";
         variant = "";
       };
-      enable = true; # Enable the X11 windowing system.
-
-# Enable the Cinnamon Desktop Environment.
-        displayManager.lightdm.enable = true;
+      enable = true;
+      libinput.enable = true;
+      displayManager.lightdm.enable = true;
       desktopManager.cinnamon.enable = true;
-
     };
     mullvad-vpn = {
       enable = true;
       package = pkgs.mullvad-vpn;
     };
   };
-
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
-
-    virtualisation.docker.enable = true;
+  system.stateVersion = "24.05";
+  virtualisation.docker.enable = true;
 }
