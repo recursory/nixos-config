e8c460a9a608a137be3cfc5e6f8526a9aa676749
recursory <olivianaomilund@gmail.com>
2025-10-25 16:30:05 -0700

    regular commit

--- configuration.nix
+++ configuration.nix
@@ -96,10 +96,15 @@
       discordchatexporter-cli
 #trenchbroom
 #qbittorrent
+      deluge
       yt-dlp
       libGL
       htop
       btop
+      trackma
+      deluge
+      libgcc
+      pulseaudio
       ];
   services = {
     flatpak.enable = true;
@@ -134,18 +139,30 @@
   fonts.fontconfig.defaultFonts = {
     monospace = [
       "DejaVu Sans Mono"
-      "IPAGothic"
+        "IPAGothic"
     ];
     sansSerif = [
       "DejaVu Sans"
-      "IPAPGothic"
+        "IPAPGothic"
     ];
     serif = [
       "DejaVu Serif"
-      "IPAPMincho"
+        "IPAPMincho"
     ];
   };
 
+  {
+# For mount.cifs, required unless domain name resolution is not needed.
+    environment.systemPackages = [ pkgs.cifs-utils ];
+    fileSystems."/mnt/share" = {
+      device = "//192.168.1.68/Public";
+      fsType = "cifs";
+      options = let
+# this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    };
+  }
 
 }
