4f252e48dd8e632ab5d62e20b0b36e9ec7b13563
recursory <olivianaomilund@gmail.com>
2025-10-25 17:05:41 -0700

    fix syntax error

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,7 @@
       deluge
       libgcc
       pulseaudio
+      cifs-utils # samba stuff
       ];
   services = {
     flatpak.enable = true;
@@ -151,18 +152,15 @@
     ];
   };
 
-  {
 # For mount.cifs, required unless domain name resolution is not needed.
-    environment.systemPackages = [ pkgs.cifs-utils ];
-    fileSystems."/mnt/share" = {
-      device = "//192.168.1.68/Public";
-      fsType = "cifs";
-      options = let
+  fileSystems."/mnt/share" = {
+    device = "//192.168.1.68/Public";
+    fsType = "cifs";
+    options = let
 # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
 
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-    };
-  }
-
+  services.gnome.gnome-keyring.enable = true;
 }
